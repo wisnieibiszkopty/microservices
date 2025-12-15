efd5ab7821a6235ffd899881123b3b3bae48731f
Kamil Wodowski <s97766@pollub.edu.pl>
2025-12-15 14:06:04 +0100

    AuthenticationService, SecurityConfig

--- accommodation-service/src/main/java/com/example/accommodationservice/auth/AuthenticationService.java
+++ accommodation-service/src/main/java/com/example/accommodationservice/auth/AuthenticationService.java
@@ -1,4 +1,106 @@
 package com.example.accommodationservice.auth;
 
+import com.example.accommodationservice.auth.dto.AuthenticationRequest;
+import com.example.accommodationservice.auth.dto.AuthenticationResponse;
+import com.example.accommodationservice.auth.dto.RegisterRequest;
+import com.example.accommodationservice.user.User;
+import com.example.accommodationservice.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+
+    // TODO remove
+    @Value("{jwt.expiration}")
+    private Long expiration;
+
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    private Map<String, Object> setClaims(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", user.getRole().name());
+        claims.put("firstname", user.getFirstname());
+        claims.put("lastname", user.getLastname());
+        return claims;
+    }
+
+    public AuthenticationResponse register(RegisterRequest request) throws Exception {
+        var existingUser = userRepository.findByEmail(request.email());
+
+        if(existingUser.isPresent()){
+            throw new Exception("User with this email already exists!");
+        }
+
+        var user = new User();
+        user.setEmail(request.email());
+        user.setFirstname(request.firstname());
+        user.setLastname(request.lastname());
+        user.setRole(request.role());
+
+        var encodedPassword = passwordEncoder.encode(request.password());
+        user.setPassword(encodedPassword);
+
+        userRepository.save(user);
+
+        var claims = setClaims(user);
+
+        var token = jwtService.generateToken(user, claims);
+        var refreshToken = jwtService.generateRefreshToken(user);
+
+        return new AuthenticationResponse(
+            token,
+            refreshToken,
+            "Bearer",
+            // TODO replace with proper value
+            LocalDateTime.now(),
+            user.getEmail(),
+            user.getRole()
+        );
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.email(),
+                        request.password()
+                )
+        );
+
+        var user = userRepository.findByEmailAndEnabledTrue(request.email());
+
+        if(user.isEmpty()){
+            throw new RuntimeException("User doesn't exist");
+        }
+
+        var existingUser = user.get();
+        var claims = setClaims(existingUser);
+
+        var token = jwtService.generateToken(existingUser, claims);
+        var refreshToken = jwtService.generateRefreshToken(existingUser);
+
+        return new AuthenticationResponse(
+                token,
+                refreshToken,
+                "Bearer",
+                // TODO replace with proper value
+                LocalDateTime.now(),
+                existingUser.getEmail(),
+                existingUser.getRole()
+        );
+    }
 }
--- accommodation-service/src/main/java/com/example/accommodationservice/auth/SecurityConfig.java
+++ accommodation-service/src/main/java/com/example/accommodationservice/auth/SecurityConfig.java
@@ -1,4 +1,102 @@
 package com.example.accommodationservice.auth;
 
+import com.example.accommodationservice.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(UserDetailsService userDetailsService){
+        return new JwtAuthenticationFilter(jwtService, userDetailsService);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS)
+                )
+                .headers(headers ->
+                    headers.xssProtection(
+                            xss -> xss.headerValue(
+                                    XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK
+                            )
+                    ).contentSecurityPolicy(
+                            cps -> cps.policyDirectives("script-src 'self'")
+                    )
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(
+                Arrays.asList("http://localhost:8080")
+        );
+
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+
+    }
 }
